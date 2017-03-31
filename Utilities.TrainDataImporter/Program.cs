9880b17b11b13850bac7f70d62e0a7b3cfefbd62
Pongrácz Lajos-Levente <pongraczlajoslevente@gmail.com>
2017-03-31 22:15:09 +0300

    Added application settings and SQLite database file.

--- Utilities.TrainDataImporter/Program.cs
+++ Utilities.TrainDataImporter/Program.cs
@@ -1,12 +1,46 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
 
 namespace Utilities.TrainDataImporter
 {
     class Program
     {
+        public static IConfigurationRoot Configuration
+        {
+            get; set;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json");
+
+            // Create the configuration object that the application will
+            // use to retrieve configuration information.
+            Configuration = builder.Build();
+
+            var connectionStringFile = Configuration["connection-string"];
+            var ingredientsFile = Configuration["ingredients-file"];
+            var compoundsFile = Configuration["compounds-file"];
+            var ingredientsCompoundsFile = Configuration["ingredients-compounds-file"];
+
+            var recipesFiles = new List<string>();
+            Configuration.GetSection("recipes-files").Bind(recipesFiles);
+
+            Console.WriteLine("APPLICATION ARGUMENTS:");
+            Console.WriteLine($"'connection-string' is '{connectionStringFile}'");
+            Console.WriteLine($"'ingredients-file' is '{ingredientsFile}'");
+            Console.WriteLine($"'compounds-file' is '{compoundsFile}'");
+            Console.WriteLine($"'ingredients-compounds-file' is '{ingredientsCompoundsFile}'");
+            Console.WriteLine();
+            foreach (var recipeFile in recipesFiles)
+            {
+                Console.WriteLine($"'recipe-file' is '{recipeFile}'");
+            }
+
+            Console.ReadLine();
         }
     }
 }
