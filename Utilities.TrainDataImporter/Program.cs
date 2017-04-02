b22ac78f747f9da6d9a0fe6df6af5624d7197602
Pongrácz Lajos-Levente <pongraczlajoslevente@gmail.com>
2017-04-02 23:09:57 +0300

    Updated the config file to support regions and added debug output to files.

--- Utilities.TrainDataImporter/Program.cs
+++ Utilities.TrainDataImporter/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace Utilities.TrainDataImporter
 {
@@ -25,6 +26,7 @@ namespace Utilities.TrainDataImporter
             var ingredientsFile = Configuration["ingredients-file"];
             var compoundsFile = Configuration["compounds-file"];
             var ingredientsCompoundsFile = Configuration["ingredients-compounds-file"];
+            var countryRegionFile = Configuration["country-region-file"];
 
             var recipesFiles = new List<string>();
             Configuration.GetSection("recipes-files").Bind(recipesFiles);
@@ -34,13 +36,43 @@ namespace Utilities.TrainDataImporter
             Console.WriteLine($"'ingredients-file' is '{ingredientsFile}'");
             Console.WriteLine($"'compounds-file' is '{compoundsFile}'");
             Console.WriteLine($"'ingredients-compounds-file' is '{ingredientsCompoundsFile}'");
+            Console.WriteLine($"'country-region-file' is '{countryRegionFile}'");
             Console.WriteLine();
             foreach (var recipeFile in recipesFiles)
             {
                 Console.WriteLine($"'recipe-file' is '{recipeFile}'");
             }
 
+            ReadFromFile(countryRegionFile);
+            ReadFromFile(compoundsFile);
+            ReadFromFile(ingredientsFile);
+            ReadFromFile(ingredientsCompoundsFile);
+            foreach (var recipeFile in recipesFiles)
+            {
+                ReadFromFile(recipeFile);
+            }
+
             Console.ReadLine();
+        }
+
+        private static void ReadFromFile(string filename)
+        {
+            Console.WriteLine($"Content of the file '{filename}':");
+            try
+            {   // Open the text file using a stream reader.
+                using (StreamReader sr = new StreamReader(File.OpenRead(filename)))
+                {
+                    // Read the stream to a string, and write the string to the console.
+                    String line = sr.ReadToEnd();
+                    Console.WriteLine(line);
+                }
+                Console.WriteLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
